d851cc38c5f05ff7eda632db76acda2e73560dec
Andre Rousselet <andre098@hotmail.com>
2023-09-11 17:43:24 -0300

    feat(meals-routes): add GET and POST /meals endpoint
    when creating a meal, add cookie as well if it doesnt exist

---
+++ src/routes/meals.ts
@@ -0,0 +1,44 @@
+import crypto from "node:crypto";
+import { FastifyInstance } from "fastify";
+import { knex } from "../database";
+import { z } from "zod";
+import { checkSessionId } from "../middlewares/check-session-id";
+
+export async function mealsRoutes(app: FastifyInstance) {
+  app.get("/", { preHandler: [checkSessionId] }, async (request, _reply) => {
+    const { sessionId } = request.cookies;
+    const meals = await knex("meals").where("session_id", sessionId).select();
+    return { meals };
+  });
+
+  app.post("/", async (request, reply) => {
+    const createMealBodySchema = z.object({
+      name: z.string().min(3),
+      description: z.string(),
+      on_diet: z.boolean(),
+      user_id: z.string().uuid(),
+    });
+    const parsedMealBody = createMealBodySchema.safeParse(request.body);
+    if (!parsedMealBody.success) {
+      return reply.status(400).send({ error: parsedMealBody.error });
+    }
+    const { name, description, on_diet, user_id } = parsedMealBody.data;
+    let sessionId = request.cookies.sessionId;
+    if (!sessionId) {
+      sessionId = crypto.randomUUID();
+      reply.cookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      });
+    }
+    await knex("meals").insert({
+      id: crypto.randomUUID(),
+      name,
+      description,
+      on_diet,
+      session_id: sessionId,
+      user_id,
+    });
+    return reply.status(201).send();
+  });
+}
